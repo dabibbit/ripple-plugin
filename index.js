3d039b6be9a91393a1b44aa17fe6cb1ef3aee193
Steven Zeiler <zeiler.steven@gmail.com>
2014-09-07 14:19:29 -0700

    [FEATURE] Expose ripple-rest router as plugin router.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-//const rippleRest = require('ripple-rest');
+const RippleRestPlugin = require('ripple-rest');
 const express = require('express');
 
 function RippleSimplePlugin(options) {
+  var rippleRest = new RippleRestPlugin(options);
   var options = options || {};
   this.gatewayd = options.gatewayd;
-  this.router = new express.Router();
+  this.router = rippleRest.router;
   this.processes = [
     __dirname+'/processes/outgoing.js',
     __dirname+'/processes/incoming.js'
